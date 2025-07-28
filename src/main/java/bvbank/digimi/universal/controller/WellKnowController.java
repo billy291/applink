06a06e60c4c6de6fc3eafaf518abc73839632c81
billy291 <khoa291@gmail.com>
2025-07-28 13:38:10 +0700

    Update WellKnowController.java

--- src/main/java/bvbank/digimi/universal/controller/WellKnowController.java
+++ src/main/java/bvbank/digimi/universal/controller/WellKnowController.java
@@ -13,14 +13,12 @@ import java.nio.charset.StandardCharsets;
 @RestController
 public class WellKnowController {
     @GetMapping(value = "/.well-known/apple-app-site-association", produces = "application/json")
-    @Cacheable(value = "wellKnownFiles", key = "'assetlinks.json'")
     public String getAppleAppSiteAssociation() throws IOException {
         Resource resource = new ClassPathResource("static/.well-known/apple-app-site-association");
         return StreamUtils.copyToString(resource.getInputStream(), StandardCharsets.UTF_8);
     }
 
     @GetMapping(value = "/.well-known/assetlinks.json", produces = "application/json")
-    @Cacheable(value = "wellKnownFiles", key = "'assetlinks.json'")
     public String getAssetLinks() throws IOException {
         Resource resource = new ClassPathResource("static/.well-known/assetlinks.json");
         return StreamUtils.copyToString(resource.getInputStream(), StandardCharsets.UTF_8);
